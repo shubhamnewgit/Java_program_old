75028880116fe0aebfac7277b166ee5ccd7a413f
Shubhu Adsod <adsodshubham95@gmail.com>
2026-02-27 16:04:29 +0530

    i have added modification

--- src/OOP_Abstraction/DemoPage.java
+++ src/OOP_Abstraction/DemoPage.java
@@ -3,7 +3,20 @@ package OOP_Abstraction;
 public class DemoPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int i = 10;
+		
+		double d = 20.20;
+		
+		double c = i+d;
+		System.out.println(c);
+		
+		String s = "Shubham";
+		
+		System.out.println("this is my first demo page push in git");
+		
+		
+		
 
 	}
 
